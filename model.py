87290eac2693a12bac2038b696bad93510d41a13
Bhavyan24 <111193847+Bhavyan24@users.noreply.github.com>
2024-03-20 11:45:33 +0530

    Update model.py

--- model.py
+++ model.py
@@ -22,7 +22,8 @@ for _ in range(50):
   print(f'MSE Result: { test_mse}')
   print("Average Mean Squared Error:", average_mse)
   with open('metrics.txt', 'w') as outfile:
-  outfile.write(f'\n Mean Squared Error = {average_mse}.')
+    outfile.write(f'\n Mean Squared Error = {average_mse}.')
+
 
 plt.scatter(X_train, y_train, color='blue', label='Training data')
 
